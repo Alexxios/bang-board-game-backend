d7cd9b3fb2650f9d7e5ffaf6753ec4c7540289e2
Sliderer <slidedr8ugs@mail.ru>
2023-12-07 23:02:28 +0300

    feat project: add project basics

---
+++ bang-board-game-backend/src/main/java/com/example/bangboardgamebackend/BangBoardGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bangboardgamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangBoardGameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangBoardGameBackendApplication.class, args);
+    }
+
+}
---
+++ bang-board-game-backend/src/test/java/com/example/bangboardgamebackend/BangBoardGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bangboardgamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangBoardGameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
